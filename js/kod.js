95296c3eaa14dca5ebc1f652a62d9e6b8245c4fb
Yasin Torun <yasintorun4680@gmail.com>
2023-10-01 14:01:27 +0300

    Added description for a javascript functions

--- js/kod.js
+++ js/kod.js
@@ -1,36 +1,63 @@
 /**
  * Copyright (c) KodChallenge.
+ * 
+ * This file is used to kodchallenge/problems repository as a submodule.
  */
 
 
 // INPUT
 
+/**
+ * Return the argument at the given index.
+ * @param {number} index
+ * @returns {string}
+ */
 const getArg = (index) => {
     return process.argv[index + 2];
 }
 
+/**
+ * Return the argument at the given index as a number.
+ * @param {number} index
+ * @returns {number}
+ */
 const getArgAsNumber = (index) => {
     return Number(getArg(index));
 }
 
-const getArgAsString = (index) => {
-    return String(getArg(index));
-}
-
+/**
+ * Return the argument at the given index as a boolean.
+ * @param {number} index
+ * @returns {boolean}
+ */
 const getArgAsBoolean = (index) => {
     const arg = getArg(index);
     return arg !== "false" && arg != '0' && Boolean(getArg(index));
 }
 
+/**
+ * Return the argument at the given index as a object.
+ * @param {number} index
+ * @returns {Object}
+ */
 const getArgAsObject = (index) => {
     return JSON.parse(getArg(index));
 }
 
+/**
+ * Return the argument at the given index as a array.
+ * @param {number} index
+ * @returns {Array}
+ */
 const getArgAsArray = (index) => {
     return getArgAsObject(index);
 }
 
-
+/**
+ * Return the argument at the given index as a date.
+ * @param {number} index
+ * @returns {Date}
+ */
 const getArgAsDate = (index) => {
     return new Date(getArg(index));
 }
@@ -38,15 +65,27 @@ const getArgAsDate = (index) => {
 
 // OUTPUT
 
-
+/**
+ * Print the given value.
+ * @param {any} value
+ */
 const print = (value) => {
     console.log(value);
 }
 
+/**
+ * Print the given value as a boolean.
+ * @param {any} value
+ */
 const printAsBoolean = (value) => {
     console.log(value !== "false" && value != '0' && Boolean(value));
 }
 
+/**
+ * Print the given value as a array.
+ * @param {any} value
+ * @example printAsArray([[1,2],[3]]) // Output => [[1,2],[3]]
+ */
 const printAsArray = (array) => {
     console.log(JSON.stringify(array));
 }
